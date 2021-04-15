b671c68a7a474bb7f1801dee3c6867b30755121f
alvarocbcp <alvarocbcp@gmail.com>
2021-04-15 20:28:15 +0200

    jjaashdvkajasghdkjbkhcbkajbdkhavbwkjdbkasvdkbaskdvwhb,ndjashvdkn ans vdm asmdvmas vddbaekh vdjasdn jvkas djavashd

---
+++ workspace-eclipse/Ejercicios_CSS_1/src/ejercicio2/MainEjercicio2.java
@@ -0,0 +1,34 @@
+package ejercicio2;
+
+import ejercicio1.MainFlowPane;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class MainEjercicio2 extends Application {
+	private FlowPane rootLayout;
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			// Carga el diseño del archivo FXML en la variable rootLayout
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainFlowPane.class.getResource("FlowPaneLayout.fxml"));
+			rootLayout = (FlowPane) loader.load();
+			rootLayout.getStylesheets().add("ejercicio2/Ejercicio2.css");
+
+			// Mostramos la escena del FlowPane de la variable rootLayot
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
