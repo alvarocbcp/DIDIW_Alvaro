5e330777bd1f0e18e36cf170b9613b8fdaea660b
alvaro <alvarocbcp@gmail.com>
2021-04-19 10:39:51 +0200

    jhgjhg

--- workspace-eclipse/Ejercicios_CSS_1/src/ejercicio2/MainEjercicio2.java
+++ workspace-eclipse/Ejercicios_CSS_1/src/ejercicio2/MainEjercicio2.java
@@ -1,22 +1,21 @@
 package ejercicio2;
 
-import ejercicio1.MainFlowPane;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainEjercicio2 extends Application {
-	private FlowPane rootLayout;
+	private BorderPane rootLayout;
 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			// Carga el diseño del archivo FXML en la variable rootLayout
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MainFlowPane.class.getResource("FlowPaneLayout.fxml"));
-			rootLayout = (FlowPane) loader.load();
+			loader.setLocation(MainEjercicio2.class.getResource("Ejercicio2.fxml"));
+			rootLayout = (BorderPane) loader.load();
 			rootLayout.getStylesheets().add("ejercicio2/Ejercicio2.css");
 
 			// Mostramos la escena del FlowPane de la variable rootLayot
